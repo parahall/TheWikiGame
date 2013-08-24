52316bd7aeedcc3e42d8b52b7e87b19a93ca41ab
Yonatan Levin <parahall@gmail.com>
2013-08-24 15:17:40 +0300

    bfs implementation

--- WikiGame-Server/cloud/bfs.js
+++ WikiGame-Server/cloud/bfs.js
@@ -1,3 +1,44 @@
-exports.bfs = function(source, getAdjFn, processNodeFn){
-	return true;
+module.exports = function(source, getAdjFn, processNodeFn, getId){
+	this.source = source;
+	this.getAdjFn = getAdjFn;
+	this.processNodeFn = processNodeFn;
+	this.getId = getId;
 }
+
+exports.process = function(){
+	var isFinished = false;
+	var queue = [];
+	queue.push(source);
+	setLength(source,0);
+	while(queue.length !== 0 & !isFinished){
+		var currentNode = queue.shift();
+		var adjNodes = getAdjFn(currentNode);
+		for (var i = 0; i < adjNodes.length; i++) {
+			if (!isMarked(getId(node))) {
+				queue.push(adjNodes[i]);
+				mark(adjNodes[i]);
+				setLength(getLength(currentNode)+1);
+			};
+		};
+		isFinished = this.processNodeFn(currentNode);
+	}
+}
+var markedObjects = [];
+var nodesLenght = {};
+function mark(node){
+	markedObjects.push(this.getId(node));
+}
+
+function isMarked(node){
+	return markedObjects.indexOf(this.getId(node))>-1;
+}
+
+function setLength(node,length){
+	node.length = length;
+	nodesLenght[this.getId(node)] = length;
+}
+
+function getLength(node){
+	return nodesLenght[nodesLenght.indexOf(this.getId(node))];
+}
+
