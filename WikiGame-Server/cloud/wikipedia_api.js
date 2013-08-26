1b63a46e7b8d2adcfdbcad21bba6ffc983b7c67c
parahall <parahall@gmail.com>
2013-08-26 22:21:03 +0300

    update random link

--- WikiGame-Server/cloud/wikipedia_api.js
+++ WikiGame-Server/cloud/wikipedia_api.js
@@ -1,6 +1,6 @@
 exports.getRandomArticle = function(success, error) {
   Parse.Cloud.httpRequest({
-	  url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&list=random',
+	  url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&list=random&rnnamespace=0',
 	  headers: {
 	    'Content-Type': 'application/json'
 	  },
